3a412d1753efb37eb09a6f2567d9c76bb785c914
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2024-03-20 14:15:45 +0100

    Add launch.json and README.md files, and implement basic command handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod commands;
+mod packages;
+mod utils;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Parse command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Handle commands
+    if args.len() > 1 {
+        let command = &args[1];
+        match command.as_str() {
+            "install" => println!("Installing nothing"),//commands::install(&args), // Handle "install" command
+            _ => println!("Unknown command: {}", command),
+        }
+    } else {
+        println!("Usage: rustedpyron <command>");
+    }
 }
